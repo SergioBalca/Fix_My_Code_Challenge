5956b57dbc92ef4e6cd96d42026112fb3d8da4bf
SergioBalca <3896@holbertonschool.com>
2022-05-26 14:38:29 -0500

    area_of_my_square method fixed

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
+"""Module with square class"""
 
-class square():
-
+class Square():
+    """ square class
+        class attributes:
+            - width: width of a square
+            - height: height of a square
+    """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ constructor """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """ Returns area of the square """
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Returns perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ str method to return str representation
+            of an square instance
+        """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
